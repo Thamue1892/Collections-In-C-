5a5a864de79e71d6ce1e2d8d358fabd679065bc3
ThaymesCoder <tmpofu@thami.co.za>
2018-02-07 23:13:17 +0200

    Remove elements at a specified index

---
+++ RemovingElementAtASpecifiedPlace/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemovingElementAtASpecifiedPlace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //Remove odd numbers
+            for (int index = numbers.Count - 1; index >= 0; index--)
+            {
+                if (numbers[index] % 2 == 1)
+                {
+                    numbers.RemoveAt(index);
+                }
+            }
+
+            numbers.ForEach(number => Console.WriteLine(number));
+        }
+    }
+}
