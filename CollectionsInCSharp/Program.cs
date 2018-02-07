a124c875d8c12d1fc7472be4d1ad53bca20d29a4
ThaymesCoder <tmpofu@thami.co.za>
2018-02-07 23:04:24 +0200

    Removing items in a list

--- CollectionsInCSharp/Program.cs
+++ CollectionsInCSharp/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace CollectionsInCSharp
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsInCSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var teamsList = new List<string> {"Manchester United", "Liverpool", "Arsenal", "Chelsea"};
+
+            foreach (var team in teamsList)
+            {
+                Console.WriteLine(team);
+            }
         }
     }
 }
---
+++ RemovingItemsInAList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemovingItemsInAList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var IDNumbers = new List<int> { 0123, 4567, 890 };
+            IDNumbers.Remove(890);
+
+            foreach (var idNumber in IDNumbers)
+            {
+                Console.WriteLine(idNumber);
+            }
+        }
+    }
+}
