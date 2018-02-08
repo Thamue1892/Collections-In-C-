2718672a3164d8dd1324d99656045f437955ef07
ThaymesCoder <tmpofu@thami.co.za>
2018-02-08 23:28:32 +0200

    Sorting a list

---
+++ SortingACollection/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SortingACollection
+{
+    public class Car : IComparable<Car>
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+
+
+        public int CompareTo(Car other)
+        {
+            int compare;
+            compare = String.Compare(this.Color, other.Color, true);
+            if (compare == 0)
+            {
+                compare = this.Speed.CompareTo(other.Speed);
+
+                //Use descending order for speed.
+                compare = -compare;
+            }
+            return compare;
+        }
+    }
+}
---
+++ SortingACollection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortingACollection
+{
+    class Program
+    {
+        private static void listCars()
+        {
+            var cars = new List<Car>
+            {
+                {new Car() {Name = "car1", Color = "blue", Speed = 20}},
+                {new Car() {Name = "car2", Color = "red", Speed = 50}},
+                {new Car() {Name = "car3", Color = "green", Speed = 10}},
+                {new Car() {Name = "car4", Color = "blue", Speed = 50}},
+                {new Car() {Name = "car5", Color = "blue", Speed = 30}},
+                {new Car() {Name = "car6", Color = "red", Speed = 60}},
+                {new Car() {Name = "car7", Color = "green", Speed = 50}}
+            };
+
+            cars.Sort();
+
+            foreach (Car thisCar in cars)
+            {
+                Console.Write(thisCar.Color.PadRight(5)+ " ");
+                Console.Write(thisCar.Speed.ToString()+" ");
+                Console.Write(thisCar.Name);
+                Console.WriteLine();
+
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
